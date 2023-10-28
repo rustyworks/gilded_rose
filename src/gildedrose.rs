a19f32910e26c8f05e414a4735c15658bc223277
Tristanto <tristanto.kurniawan@gmail.com>
2023-10-28 23:51:45 +0700

    Part 1 - Add gilded rose test (Unrefined)

--- src/gildedrose.rs
+++ src/gildedrose.rs
@@ -89,11 +89,135 @@ mod tests {
     use super::{GildedRose, Item};
 
     #[test]
-    pub fn foo() {
-        let items = vec![Item::new("foo", 0, 0)];
+    pub fn normal_item() {
+        let items = vec![Item::new("Dexterity Vest", 10, 20)];
         let mut rose = GildedRose::new(items);
+
         rose.update_quality();
 
-        assert_eq!("fixme", rose.items[0].name);
+        assert_eq!(9, rose.items[0].sell_in);
+        assert_eq!(19, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn normal_item_date_passed() {
+        let items = vec![Item::new("Dexterity Vest", 0, 20)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(-1, rose.items[0].sell_in);
+        assert_eq!(18, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn quality_minimum_at_0() {
+        let items = vec![Item::new("Dexterity Vest", 10, 0)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(9, rose.items[0].sell_in);
+        assert_eq!(0, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn aged_brie_increase_quality() {
+        let items = vec![Item::new("Aged Brie", 10, 20)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(9, rose.items[0].sell_in);
+        assert_eq!(21, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn aged_brie_increase_quality_twice_when_date_passed() {
+        let items = vec![Item::new("Aged Brie", 0, 20)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(-1, rose.items[0].sell_in);
+        assert_eq!(22, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn quality_maximum_at_50() {
+        let items = vec![Item::new("Aged Brie", 10, 50)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(9, rose.items[0].sell_in);
+        assert_eq!(50, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn sulfuras_never_sold_never_decrease_in_quality() {
+        let items = vec![Item::new("Sulfuras, Hand of Ragnaros", 10, 50)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(10, rose.items[0].sell_in);
+        assert_eq!(50, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn back_stage_pass_normal_date() {
+        let items = vec![Item::new("Backstage passes to a TAFKAL80ETC concert", 15, 20)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(14, rose.items[0].sell_in);
+        assert_eq!(21, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn back_stage_pass_less_than_10_days_and_5_days_or_more() {
+        let items = vec![Item::new("Backstage passes to a TAFKAL80ETC concert", 10, 20)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(9, rose.items[0].sell_in);
+        assert_eq!(22, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn back_stage_pass_less_than_5_days() {
+        let items = vec![Item::new("Backstage passes to a TAFKAL80ETC concert", 5, 20)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(4, rose.items[0].sell_in);
+        assert_eq!(23, rose.items[0].quality);
+    }
+
+    #[test]
+    pub fn back_stage_pass_after_concert() {
+        let items = vec![Item::new("Backstage passes to a TAFKAL80ETC concert", 0, 20)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(-1, rose.items[0].sell_in);
+        assert_eq!(0, rose.items[0].quality);
+    }
+
+    #[ignore]
+    #[test]
+    pub fn conjured_item() {
+        let items = vec![Item::new("Conjured Item", 10, 20)];
+        let mut rose = GildedRose::new(items);
+
+        rose.update_quality();
+
+        assert_eq!(9, rose.items[0].sell_in);
+        assert_eq!(18, rose.items[0].quality);
     }
 }
